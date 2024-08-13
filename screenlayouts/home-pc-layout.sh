18ae93731d746bb658f1d06cd82e54e06d013a18
Noe Trevino <noetrevino.music@gmail.com>
2024-08-12 20:26:34 -0500

    delete screenlayout
    wrong place

--- screenlayouts/home-pc-layout.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-xrandr --output HDMI-0 --off --output DP-0 --mode 1920x1080 --pos 0x0 --rotate left --output DP-1 --off --output DP-2 --mode 3440x1440 --pos 1080x480 --rotate normal --output DP-3 --off --output DP-4 --off --output DP-5 --off
